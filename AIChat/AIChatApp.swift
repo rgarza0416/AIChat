98e93fb553358084b44e408d708310b546be8ee1
Ricardo Garza <rgarza0416@yahoo.com>
2025-03-27 21:47:24 -0500

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Ricardo Garza on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
