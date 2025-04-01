d43f98bd1ba79d848f113a1de764e59418f6e13e
Ricardo Garza <rgarza0416@yahoo.com>
2025-04-01 16:05:34 -0500

    OnboardingIntroView, OnboardingColorView

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -39,7 +39,7 @@ struct WelcomeView: View {
     private var ctaButtons: some View {
         VStack(spacing: 8) {
             NavigationLink {
-                OnboardingCompletedView()
+                OnboardingIntroView()
             } label: {
                 Text("Get Started")
                     .callToActionButton()
