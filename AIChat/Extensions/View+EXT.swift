fd470da5a4e5ad140135c735dffa1b0ba55bd743
Ricardo Garza <rgarza0416@yahoo.com>
2025-04-02 13:36:03 -0500

    CategoryCell

--- AIChat/Extensions/View+EXT.swift
+++ AIChat/Extensions/View+EXT.swift
@@ -20,7 +20,25 @@ extension View {
     }
     
     func tappableBackground() -> some View {
+        background(Color.black.opacity(0.0001))
+    }
+    
+    func removeListRowFormatting() -> some View {
         self
-        .background(Color.black.opacity(0.0001))
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .listRowBackground(Color.clear)
+    }
+    
+    func addingGradientBackgroundForText() -> some View {
+        background(
+            LinearGradient(
+                colors: [
+                    Color.black.opacity(0),
+                    Color.black.opacity(0.3),
+                    Color.black.opacity(0.4)
+                ],
+                startPoint: .top,
+                endPoint: .bottom)
+        )
     }
 }
