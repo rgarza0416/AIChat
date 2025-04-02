c41f6a548c8f3874117f77c13788e02ec7caeff6
Ricardo Garza <rgarza0416@yahoo.com>
2025-04-02 16:16:02 -0500

    ChatModel, ChatMessageModel

---
+++ AIChat/Extensions/Date+EXT.swift
@@ -0,0 +1,16 @@
+//
+//  Date+EXT.swift
+//  AIChat
+//
+//  Created by Ricardo Garza on 4/2/25.
+//
+import Foundation
+
+extension Date {
+    func addingTimeInterval(days: Int = 0, hours: Int = 0, minutes: Int = 0) -> Date {
+        let dayInterval = TimeInterval(days * 24 * 60 * 60)
+        let hourInterval = TimeInterval(hours * 60 * 60)
+        let minuteInterval = TimeInterval(minutes * 60)
+        return self.addingTimeInterval(dayInterval + hourInterval + minuteInterval)
+    }
+}
